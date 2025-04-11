d938e658279df0e6ba3fca6161654588baf2e5e3
rosfandy <brosfandy@gmail.com>
2025-04-11 14:48:13 +0700

    update vercel config

--- src/components/navbar/MainNavbar.tsx
+++ src/components/navbar/MainNavbar.tsx
@@ -26,7 +26,7 @@ export const MainNavbar = () => {
             <NavbarLayout setDropdown={setIsDropdown} isDropdown={isDropdown}>
                 <div className="md:flex items-center gap-x-8 hidden">
                     <Link to="/category" className="">Kategori</Link>
-                    <img onClick={() => setIsDropdown(!isDropdown)} className='w-[44px] h-auto rounded-xl ' src="https://s3-alpha-sig.figma.com/img/0577/f0e9/b7fca2f32639871454da0de95f951709?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=CuOr3Yc7otSDkyXdEJJHE3gBMHMhsavmFIEOzSpMkZlmIBXuJXm7FIWovYF7HMwbio74vx3fK3RjU8hhPiZEm5GJF7fN0XjTI3DshS1SsrTZuHzTMTW7UfbqNms-uqznpdsJ0dylpeaJ2h2UD32ZYpth6R7Mi943YLr3oje1YfYQgs69juWB~8-ZSOv5qVww7qY39lNP5IhCf5ZNgmEPi4~VHIVkvFptLDzlHWkIiewgupwCHSmwoXyYZvkm75ggrEYIpkEZGvL~eGrct4hzKz-Zf4vBn27j5Ypk6UY8IRFp2mhDbKKH0fza7Jo1RRGhpScIt4eAL1El81HHBzvjDg__" alt="profile" />
+                    <img onClick={() => setIsDropdown(!isDropdown)} className='w-[44px] h-auto rounded-xl cursor-pointer' src="https://s3-alpha-sig.figma.com/img/0577/f0e9/b7fca2f32639871454da0de95f951709?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=CuOr3Yc7otSDkyXdEJJHE3gBMHMhsavmFIEOzSpMkZlmIBXuJXm7FIWovYF7HMwbio74vx3fK3RjU8hhPiZEm5GJF7fN0XjTI3DshS1SsrTZuHzTMTW7UfbqNms-uqznpdsJ0dylpeaJ2h2UD32ZYpth6R7Mi943YLr3oje1YfYQgs69juWB~8-ZSOv5qVww7qY39lNP5IhCf5ZNgmEPi4~VHIVkvFptLDzlHWkIiewgupwCHSmwoXyYZvkm75ggrEYIpkEZGvL~eGrct4hzKz-Zf4vBn27j5Ypk6UY8IRFp2mhDbKKH0fza7Jo1RRGhpScIt4eAL1El81HHBzvjDg__" alt="profile" />
 
                 </div>
 
